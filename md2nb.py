82ea8f033425a5d7052d18b3e0e86331e6292ba8
Nick Ahn <nickahn01@gmail.com>
2024-03-31 03:49:17 -0700

    Add h4 case

--- md2nb.py
+++ md2nb.py
@@ -1,6 +1,7 @@
 # parse.py: Parse markdown sections into jupyter notebook
 import re
 import nbformat
+import argparse
 
 
 def pattern_check(line: str) -> bool:
@@ -17,13 +18,13 @@ def pattern_check(line: str) -> bool:
     header_levels = [
         '#',
         '##',
-        '###'
+        '###',
+        '####'
         ]
     pattern = ''
     for header in header_levels:
         pattern = fr'^{header}\s+(.*?)\s*$'
         if re.match(pattern, line):
-            print('Check!', line)
             return True
 
     # Look for ordered lists in line (e.g.: "1.")
@@ -33,41 +34,53 @@ def pattern_check(line: str) -> bool:
 
     return False
 
+def md2nb(input_file: str, output_file: str):
+    """
+    Parse Markdown file sections and create Jupyter Notebook.
 
-# Parse markdown sections
-input_file = 'classwork17.md'
-with open(input_file, 'r') as f:
-    # Add Markdown cells
+    Params:
+    * input_file: path to .md file to be parsed
+    * output_file: path to save .ipynb file
+    """
     cells = []
-    markdown_content = ''
-    in_code_block = False
-    for line in f:
-        if re.match(r"```", line):
-            # Check for code block start
-            print("code block!")
-            in_code_block = not in_code_block
-            markdown_content += line
-            continue
-        elif in_code_block:
-            # if inside code block, continue
-            markdown_content += line
-            continue
-        elif pattern_check(line):
-            # end concatenation. Create markdown cell and start over
-            cells.append(nbformat.v4.new_markdown_cell(source=markdown_content))
-            markdown_content = '' + line
-        else:
-            markdown_content += line
-    
-    cells.append(nbformat.v4.new_markdown_cell(source=markdown_content))
+    # Parse markdown sections
+    with open(input_file, 'r') as f:
+        markdown_content = ''
+        in_code_block = False
+        for line in f:
+            if re.match(r"```", line):
+                # Check for code blocks to not confuse python comments with headers
+                in_code_block = not in_code_block
+                markdown_content += line
+                continue
+            elif in_code_block:
+                markdown_content += line
+                continue
+            elif pattern_check(line):
+                cells.append(nbformat.v4.new_markdown_cell(source=markdown_content))
+                markdown_content = '' + line
+            else:
+                markdown_content += line
+        
+        # Append last markdown content
+        cells.append(nbformat.v4.new_markdown_cell(source=markdown_content))
 
-# Create new jupyter notebook
-notebook = nbformat.v4.new_notebook()
-notebook['cells'] = cells
+    notebook = nbformat.v4.new_notebook()
+    notebook['cells'] = cells
 
-output_file = 'test.ipynb'
-with open(output_file, 'w') as f:
-    nbformat.write(notebook, f)
-    
-print(f"Conversion Successful! Jupyter Notebook saved to {output_file}")
+    with open(output_file, 'w') as f:
+        nbformat.write(notebook, f)
+        
+    print(f"Conversion Successful! Jupyter Notebook saved to {output_file}")
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='md2nb',
+        description='Convert Markdown headers to Jupyter Notebooks cells'
+        )
+    parser.add_argument('input_file', type=str, help='path to .md file to be parsed')
+    parser.add_argument('output_file', type=str, help='path to save .ipynb file')
+    args = parser.parse_args()
+
+    md2nb(input_file=args.input_file, output_file=args.output_file)
